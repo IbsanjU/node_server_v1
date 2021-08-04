e4fb25b750435d1a091c225787371081520c3801
IbsanjU <bharath2668@gmail.com>
2021-08-04 18:36:39 +0530

    insert and fetch from table

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
-const express = require("express")
+const express = require('express')
 const app = express()
 
-const config = require("./config")
+const db = require('./models')
 
-const db = require("./models")
+const { User } = require('./models') // get user from models
+
+app.get('/select', (req, res) => {
+  User.findAll()
+    .then((users) => {
+      res.send(users)
+    })
+    .catch((err) => {
+      if (err) res.send(err)
+    })
+  res.send('select')
+})
+
+app.get('/insert', (req, res) => {
+  User.create({
+    firstName: 'Bharath',
+    aga: 26,
+  }).then((err, res) => {
+    if (err) res.send('err : ' + err)
+    res.send(err)
+  })
+})
+
+app.get('/delete', (req, res) => {
+  User.destroy({ where: { id: 01 } })
+  res.send('delete')
+})
 
 db.sequelize.sync().then((req) => {
-    app.listen(3001, () => console.log("server is running...!"))
+  app.listen(3001, () => console.log('server is running...!'))
 })
 
 /* 
